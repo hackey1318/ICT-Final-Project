e3310daf89af07d97f456dfe81e9b1e05f62c0cd
yesim <hackey1318@naver.com>
2025-04-04 09:40:53 +0900

    CGV 영화관 좌표 정보 조회 로직 추가

--- finalProject/src/main/java/com/ict/finalProject/testCode/CGVTheaterCrawler.java
+++ finalProject/src/main/java/com/ict/finalProject/testCode/CGVTheaterCrawler.java
@@ -5,13 +5,57 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CGVTheaterCrawler {
+
+    private static final String KAKAO_API_KEY = "47d3d6c7b00ecb8beb627eeafe04d4a9"; // 본인 API 키 사용
+//    private static final String KAKAO_API_KEY = "83d1dc7f3cbc27e375262210a7b0bdeb"; // 본인 API 키 사용
+
+    public static void getGeoInfo(List<String> theaterNameList) {
+
+        String apiUrl = "https://dapi.kakao.com/v2/local/search/keyword.json?query=";
+
+        for (String theaterName : theaterNameList) {
+
+            if (theaterName.contains("(임시휴업)")) {
+                theaterName = theaterName.replace("(임시휴업)", "");
+            } else if (theaterName.contains("CINE de CHEF")) {
+                theaterName = theaterName.replace("CINE de CHEF", "씨네드쉐프");
+            }
+            try {
+                System.out.print(theaterName + " 좌표: ");
+                URL url = new URL(apiUrl + URLEncoder.encode(theaterName, "UTF-8"));
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Authorization", "KakaoAK " + KAKAO_API_KEY);
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String response = br.readLine();
+                br.close();
+
+                // JSON 파싱
+                JSONObject json = new JSONObject(response);
+                JSONObject location = json.getJSONArray("documents").getJSONObject(0);
+                String latitude = location.getString("y");
+                String longitude = location.getString("x");
+
+                System.out.println(latitude + ", " + longitude);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         String url = "http://www.cgv.co.kr/theaters/";
@@ -37,8 +81,9 @@ public class CGVTheaterCrawler {
                     }
                 }
                 // lat, lng 정보 parsing 후 -> DB 저장
-
                 System.out.println(theaterNameList);
+                getGeoInfo(theaterNameList);
+
             } else {
                 System.out.println("JSON 데이터를 찾을 수 없습니다.");
             }
