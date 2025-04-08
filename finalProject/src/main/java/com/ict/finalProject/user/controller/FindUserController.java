142919655646df8c2694fabb4cecfcff5e887019
blackusb <smpaa@naver.com>
2025-04-08 16:31:25 +0900

    아이디 찾기 백엔드 코드 수정. request, response 객체 따로 만들어서 반영. 예외 Path 등록. System.out 삭제.

--- finalProject/src/main/java/com/ict/finalProject/user/controller/FindUserController.java
+++ finalProject/src/main/java/com/ict/finalProject/user/controller/FindUserController.java
@@ -2,15 +2,12 @@ package com.ict.finalProject.user.controller;
 
 import com.ict.finalProject.domain.constant.StatusInfo;
 import com.ict.finalProject.oauth.repository.domain.Users;
+import com.ict.finalProject.user.controller.request.UserFindRequest;
+import com.ict.finalProject.user.controller.response.UserFindResponse;
 import com.ict.finalProject.user.service.FindUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
@@ -18,36 +15,28 @@ public class FindUserController {
     private final FindUserService findUserService;
 
     @PostMapping("/findIdOk")
-    public ResponseEntity<Map<String, Object>> findIdOk(@RequestBody Users user) {
-        //결과를 담을 맵 객체 생성
-        Map<String, Object> response = new HashMap<>();
+    public UserFindResponse findIdOk(@RequestBody UserFindRequest userFindRequest) {
+        //결과를 담을 객체 생성
+        UserFindResponse response = new UserFindResponse();
 
-        //System.out.println(user);
-        //System.out.println(user.getNickname());
-        //System.out.println(user.getEmail());
-
-        Users result = findUserService.FindId(user);
-
-        System.out.println("result=> " + result);
-        //System.out.println("result.getStatus()=> " + result.getStatus());
-        //System.out.println("result.getId()=> " + result.getId());
+        Users result = findUserService.FindId(userFindRequest);
 
         //일치하는 닉네임, 이메일이 없을 때
         if(result == null){
-            response.put("status", "idNone");
-            return ResponseEntity.ok(response);
+            response.setStatus("idNone");
+            return response;
         }
 
         //일치하는 닉네임, 이메일이 있을 때
         if (StatusInfo.ACTIVE == result.getStatus()) {
             //아이디가 존재하고 활성 상태일 때
-            response.put("status", "idActive");
-            response.put("id", result.getId());
+            response.setStatus("idActive");
+            response.setId(result.getId());
         } else if (StatusInfo.DELETE == result.getStatus()) {
             //아이디가 존재하고 비활성 상태일 때
-            response.put("status", "idDelete");
+            response.setStatus("idDelete");
         }
 
-        return ResponseEntity.ok(response);
+        return response;
     }
 }
