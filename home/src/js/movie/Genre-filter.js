01f9c7241d2fbf22def2e1631ef4ad36f8a11a74
yesim <hackey1318@naver.com>
2025-04-11 18:48:07 +0900

    영화 List API 연동 페이지 구현

---
+++ home/src/js/movie/Genre-filter.js
@@ -0,0 +1,27 @@
+import "../../css/movie/GenreFilter.css";
+
+const genres = [
+  { id: "Fantasy", name: "환타지" },
+  { id: "Romance", name: "로맨스" },
+  { id: "Crime", name: "범죄" },
+  { id: "Drama", name: "드라마" },
+  { id: "Action", name: "액션" },
+  { id: "Horror", name: "호러" },
+  { id: "Animation", name: "애니메이션" },
+];
+
+export default function GenreFilter({ selectedGenre, onSelect }) {
+  return (
+    <div className="genre-filter">
+      {genres.map((genre) => (
+        <button
+          key={genre.id}
+          className={`genre-button ${selectedGenre === genre.id ? 'active' : ''}`}
+          onClick={() => onSelect(genre)}
+        >
+          {genre.id}
+        </button>
+      ))}
+    </div>
+  );
+}
