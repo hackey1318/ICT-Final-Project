da26e46350fc55078c605134aa43c484f0d871f4
yesim <hackey1318@naver.com>
2025-04-11 14:24:23 +0900

    관리자 로그인 페이지 구현

--- home/src/App.js
+++ home/src/App.js
@@ -10,6 +10,7 @@ import KakaoCallback from './page/user/KakaoCallback';
 import FindId from './page/user/FindId';
 import FindPwd from './page/user/FindPwd';
 import PwdReset from './page/user/PwdReset';
+import ManagerLogin from './page/admin/ManagerLogin';
 
 function App() {
   return (
@@ -23,11 +24,11 @@ function App() {
           <Route path='user/findPwd' element={<FindPwd />} />
           <Route path='user/pwdReset' element={<PwdReset />} />
         </Route>
-        <Route path='/manager' element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path='login' element={<KakaoLogin />} />
-          <Route path='kakao/callback' element={<KakaoCallback />} />
+        <Route path='/manager' element={<ManagerLogin />}>
+          <Route index element={<ManagerLogin />} />
           <Route path='user/findId' element={<FindId />} />
+          <Route path='user/findPwd' element={<FindPwd />} />
+          <Route path='user/pwdReset' element={<PwdReset />} />
         </Route>
       </Routes>
     </BrowserRouter>
