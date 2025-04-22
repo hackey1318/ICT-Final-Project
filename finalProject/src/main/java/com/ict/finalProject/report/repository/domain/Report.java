73257075a3551c858763a5987bd8d7f46d689b67
sung0916 <qjeltk52@naver.com>
2025-04-22 14:15:12 +0900

    관리자 문의 페이지 구현 완료 - 신고기능 구현 예정

---
+++ finalProject/src/main/java/com/ict/finalProject/report/repository/domain/Report.java
@@ -0,0 +1,47 @@
+package com.ict.finalProject.report.repository.domain;
+
+import com.ict.finalProject.domain.constant.InquiryProceed;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@Table
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int no;  //신고 번호
+
+    private int userNo;  //유저번호
+
+    @Column(nullable = false)
+    private String subject;  //신고 제목
+
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private InquiryProceed proceed;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;  //문의생성일
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;  //문의수정일
+}
