19bc8c24d2fbcc13fb0f0a27eeaea798bda291e0
yesim <hackey1318@naver.com>
2025-04-22 09:59:53 +0900

    시네메이트 등록 에러 수정 및 UI 수정

---
+++ finalProject/src/main/java/com/ict/finalProject/cinemate/controller/request/CineMateRequest.java
@@ -0,0 +1,29 @@
+package com.ict.finalProject.cinemate.controller.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CineMateRequest {
+
+    private Integer userNo;
+
+    private Integer movieNo;
+
+    private Integer theaterNo;
+
+    private Integer maxMemberCount;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime meetingDate;
+
+    private String content;
+}
