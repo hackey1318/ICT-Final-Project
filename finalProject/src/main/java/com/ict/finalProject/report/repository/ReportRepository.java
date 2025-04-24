f41c13dbccceb60e919189aa5240407302e669a8
sung0916 <qjeltk52@naver.com>
2025-04-24 18:08:54 +0900

    <예정> - 관리자페이지 리스트 구현 - 리스트에 처리상황 변경 구현 - 리스트를 토글방식으로 신고된자/신고한자 리스트 분할
    <완료>
    - 프론트 영화리뷰에 신고 모달 구현
    - 신고하기 기능 구현(DB저장까지)

--- finalProject/src/main/java/com/ict/finalProject/report/repository/ReportRepository.java
+++ finalProject/src/main/java/com/ict/finalProject/report/repository/ReportRepository.java
@@ -2,10 +2,11 @@ package com.ict.finalProject.report.repository;
 
 import com.ict.finalProject.report.repository.domain.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
-
 
 }
