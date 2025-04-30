7a0b969fc0289994d877b9d4971b5da0d4979a49
sung0916 <qjeltk52@naver.com>
2025-04-30 11:34:53 +0900

    문의페이지 CSS수정

--- home/src/page/inquiry/InquiryPage.js
+++ home/src/page/inquiry/InquiryPage.js
@@ -219,7 +219,7 @@ function InquiryPage() {
                 </div>
                 <div style={{textAlign: 'right', 
                              minWidth: '850px',
-                             marginTop: (inquiryList.length === 0 || !inquiryList.some(item => item.status === "ACTIVE")) ? '50px' : 0}}>
+                             marginTop: (inquiryList.length === 0 || !inquiryList.some(item => item.status === "ACTIVE")) ? '70px' : 0}}>
                     <button id="write" 
                         title='문의하기' 
                         onClick={handleWriteClick}
