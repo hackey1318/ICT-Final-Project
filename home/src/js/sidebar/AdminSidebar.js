15f18684243ac54d799c120b50613eec3130a4ef
yesim <hackey1318@naver.com>
2025-05-02 16:10:17 +0900

    수정 요청 사항 반영

--- home/src/js/sidebar/AdminSidebar.js
+++ home/src/js/sidebar/AdminSidebar.js
@@ -135,6 +135,7 @@ function AdminSidebar({ activeMenu }) {
                             <ul className="admin-sub-menu">
                                 <li><Link to="/manager/home/mdlists" className={`admin-nav-link ${activeMenu === 'mdlists' ? 'active' : ''}`}>상품 목록 조회</Link></li>
                                 <li><Link to="/manager/home/ordermanage" className={`admin-nav-link ${activeMenu === 'ordermanage' ? 'active' : ''}`}>상품 구매 정보 조회</Link></li>
+                                <li><Link to="/manager/home/mdsales" className={`admin-nav-link ${activeMenu === 'mdsales' ? 'active' : ''}`}>상품 매출 조회</Link></li>
                                 <li><Link to="/manager/home/goods-like" className={`admin-nav-link ${activeMenu === 'goods-like' ? 'active' : ''}`}>상품별 찜</Link></li>
                             </ul>
                         )}
